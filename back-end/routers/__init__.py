241b5c24c7a58c591adc04ddd10fd303172dcace
jiangMo09 <mmiao0641@gmail.com>
2024-08-16 21:02:53 +0800

    [FIX]api routers renamed

--- back-end/routers/__init__.py
+++ back-end/routers/__init__.py
@@ -6,8 +6,8 @@ from .leetcode.code import router as code_router
 from .leetcode.data_input import router as data_input_router
 
 api_router = APIRouter()
-api_router.include_router(description_router, prefix="/api", tags=["leetcode"])
-api_router.include_router(code_router, prefix="/api", tags=["leetcode"])
-api_router.include_router(data_input_router, prefix="/api", tags=["leetcode"])
+api_router.include_router(description_router, tags=["leetcode"])
+api_router.include_router(code_router, tags=["leetcode"])
+api_router.include_router(data_input_router, tags=["leetcode"])
 
 __all__ = ["api_router"]
